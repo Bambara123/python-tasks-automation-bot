ce43823c6bb4a5fa5d7054dd8cae708291e4fabc
Bambara123 <e19034@eng.pdn.ac.lk>
2024-03-31 04:22:17 +0530

    mouse movement completed

--- m_movement.py
+++ m_movement.py
@@ -5,25 +5,29 @@ import time
 import pyautogui
 
 
-# generate random amount of points between two points - which represents
-# abnormal behavior of mouse.
+# generate random amount of points between two points - which represents abnormal behavior.
 def generate_points_abnormal(point_1, point_2):
-    num_points = random.choices([0, 1, 2, 3], weights=[30, 30, 20, 20], k=1)[0]
+    # tested
+    num_points = random.choices(
+        [1, 2, 3, random.randint(4, 7)], weights=[64, 25, 7, 4], k=1
+    )[0]
     points = []
 
-    if num_points > 0:
-        for _ in range(num_points):
-            x = random.randint(min(point_1[0], point_2[0]), max(point_1[0], point_2[0]))
-            y = random.randint(min(point_1[1], point_2[1]), max(point_1[1], point_2[1]))
-            point = (x, y)
-            points.append(point)
+    for _ in range(num_points):
+        x = random.randint(min(point_1[0], point_2[0]), max(point_1[0], point_2[0]))
+        y = random.randint(min(point_1[1], point_2[1]), max(point_1[1], point_2[1]))
+        point = (x, y)
+        points.append(point)
 
     return points
 
 
-# generate random amount of points between two points - normal.
+# generate random amount of points between two points - which represents normal behavior.
 def generate_points_normal(point_1, point_2):
-    num_points = random.choices([0, 1, 2, 3], weights=[60, 30, 10, 10], k=1)[0]
+    # tested
+    num_points = random.choices(
+        [0, 1, 2, 3, random.randint(4, 7)], weights=[64, 25, 7, 3, 1], k=1
+    )[0]
     points = []
 
     if num_points > 0:
@@ -42,22 +46,24 @@ def generate_points_normal(point_1, point_2):
     return points
 
 
-# Example usage:
-point_1 = (100, 200)
-point_2 = (800, 800)
-# print(generate_points_abnormal(point_1, point_2))
+# generate random amount of points in anywhere on screen.
+def generate_points_anywhere(point_1=(0, 0), point_2=(1920, 1080)):
+    # tested
+    num_points = random.choices([1, 2, 3], weights=[60, 20, 20], k=1)[0]
+    points = []
 
+    for _ in range(num_points):
+        x = random.randint(min(point_1[0], point_2[0]), max(point_1[0], point_2[0]))
+        y = random.randint(min(point_1[1], point_2[1]), max(point_1[1], point_2[1]))
+        point = (x, y)
+        points.append(point)
 
-# Example usage:
-# point_1 = (100, 200)
-# point_2 = (800, 800)
-# print(generate_points(point_1, point_2))
+    return points
 
 
 def go_to_point2(point_2):
 
     # pass point as tuple
-
     hc = HumanClicker()
 
     x = point_2[0]
@@ -67,6 +73,7 @@ def go_to_point2(point_2):
     hc.move((x, y), mouse_move_time())
 
 
+# generate move time for mouse.
 def mouse_move_time():
     # tested
     # 90% of the time, generate a value between 0.3 and 0.8
@@ -79,6 +86,7 @@ def mouse_move_time():
     return move_time
 
 
+# generate random sleep time between mouse movements between 2 points.
 def mouse_move_sleep_time():
 
     # tested
@@ -94,27 +102,34 @@ def mouse_move_sleep_time():
     return move_sleep_time
 
 
-# print(go_to_point2((800, 800)))
-
-point_1 = (100, 200)
-point_2 = (800, 800)
-
-
 # generate random amount of points to go. With bias amount.
-def generate_points_with_bias(bias_to_function1, point_1, point_2):
+# this returns the points to go through.
+def generate_points_with_bias(bias_to_function1, bias_to_function3, point_1, point_2):
     functions = [
         generate_points_normal,
         generate_points_abnormal,
+        generate_points_anywhere,
     ]
     chosen_function = random.choices(
-        functions, weights=[bias_to_function1, 100 - bias_to_function1], k=1
+        functions,
+        weights=[
+            bias_to_function1,
+            100 - bias_to_function1 - bias_to_function3,
+            bias_to_function3,
+        ],
+        k=1,
     )[0]
-    return chosen_function(point_1, point_2)
+
+    # Check if the chosen function is the one that takes no arguments
+    if chosen_function == generate_points_anywhere:
+        return chosen_function()
+    else:
+        return chosen_function(point_1, point_2)
 
 
 # this takes arguments as point_1 and point_2. and go to point_2 from point_1.
 def go_to_point1_to_point2(point_1, point_2):
-    points = generate_points_with_bias(90, point_1, point_2)
+    points = generate_points_with_bias(75, 5, point_1, point_2)
 
     time.sleep(mouse_move_sleep_time())
     for point in points:
@@ -126,9 +141,12 @@ def go_to_point1_to_point2(point_1, point_2):
 
 # return the current mouse position.
 def get_mouse_position():
+    # tested.
     position = pyautogui.position()
+    print(position.x, position.y)
     return (position.x, position.y)
 
 
 time.sleep(2)
-print(go_to_point1_to_point2(get_mouse_position(), point_2))
+
+go_to_point1_to_point2(get_mouse_position(), (400, 990))
--- test3.py
+++ test3.py
@@ -1,6 +1,47 @@
 import random
 
+
+# generate random amount of points between two points - which represents abnormal behavior.
 def generate_points_abnormal(point_1, point_2):
+    num_points = random.choices(
+        [1, 2, 3, random.randint(4, 10)], weights=[59, 20, 20, 1], k=1
+    )[0]
+    points = []
+
+    for _ in range(num_points):
+        x = random.randint(min(point_1[0], point_2[0]), max(point_1[0], point_2[0]))
+        y = random.randint(min(point_1[1], point_2[1]), max(point_1[1], point_2[1]))
+        point = (x, y)
+        points.append(point)
+
+    return points
+
+
+# generate random amount of points between two points - which represents normal behavior.
+def generate_points_normal(point_1, point_2):
+    num_points = random.choices(
+        [0, 1, 2, 3, random.randint(4, 10)], weights=[69, 25, 10, 5, 1], k=1
+    )[0]
+    points = []
+
+    if num_points > 0:
+        last_point = point_1
+        for _ in range(num_points):
+            x = random.randint(
+                min(last_point[0] + 1, point_2[0]), max(last_point[0] + 1, point_2[0])
+            )
+            y = random.randint(
+                min(last_point[1] + 1, point_2[1]), max(last_point[1] + 1, point_2[1])
+            )
+            point = (x, y)
+            points.append(point)
+            last_point = point
+
+    return points
+
+
+# generate random amount of points in anywhere on screen.
+def generate_points_anywhere(point_1=(0, 0), point_2=(1920, 1080)):
     num_points = random.choices([1, 2, 3], weights=[60, 20, 20], k=1)[0]
     points = []
 
@@ -13,23 +54,4 @@ def generate_points_abnormal(point_1, point_2):
     return points
 
 
-def generate_points_normal(point_1, point_2):
-    num_points = random.choices([1, 2, 3], weights=[60, 20, 20], k=1)[0]
-    points = []
-
-    last_point = point_1
-    for _ in range(num_points):
-        x = random.randint(
-            min(last_point[0] + 1, point_2[0]), max(last_point[0] + 1, point_2[0])
-        )
-        y = random.randint(
-            min(last_point[1] + 1, point_2[1]), max(last_point[1] + 1, point_2[1])
-        )
-        point = (x, y)
-        points.append(point)
-        last_point = point
-
-    return points
-
-
-print(generate_points_normal((1000, 200), (1200, 1800)))
+print(generate_points_anywhere())
