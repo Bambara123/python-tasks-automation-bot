1a20238d94ad048c6f1aed15e2bcad69cc96290b
Bambara123 <e19034@eng.pdn.ac.lk>
2024-04-09 23:23:58 +0530

    keyboard almost done.

--- k_typing.py
+++ k_typing.py
@@ -7,17 +7,17 @@ import ctypes
 
 # make the time delay between key presses. between (0.2, 0.5) seconds -> 80%, (0.5, 1) seconds -> 10%, (1, 10) seconds -> 10%
 def make_time_delay_normal():
-    delay_ranges = [(0.2, 0.5), (0.5, 1), (1, 10)]
-    chosen_range = random.choices(delay_ranges, weights=[80, 10, 10], k=1)[0]
+    delay_ranges = [(0, 0.3), (0.3, 1), (1, 10)]
+    chosen_range = random.choices(delay_ranges, weights=[85, 10, 5], k=1)[0]
 
     sleep_time = random.uniform(*chosen_range)
 
     return sleep_time
 
 
-# make the time delay between wrong key presses. between (0.2, 0.5) seconds -> 90%, (0.5, 1) seconds -> 10%
+# make the time delay between wrong key presses. between (0.2, 0.5) seconds -> 90%, (0.5, 5) seconds -> 10%
 def make_time_delay_wrong():
-    delay_ranges = [(0.2, 0.5), (0.5, 1)]
+    delay_ranges = [(0.2, 0.5), (0.5, 5)]
     chosen_range = random.choices(delay_ranges, weights=[90, 10], k=1)[0]
 
     sleep_time = random.uniform(*chosen_range)
@@ -30,8 +30,6 @@ def make_mistakes(last_letter):
 
     last_letter_upper = last_letter.isupper()
 
-    print(last_letter_upper)
-
     # Will type letters wrongly between 1 to 3.
     amount_of_mistakes = random.randint(1, 3)
 
@@ -40,15 +38,12 @@ def make_mistakes(last_letter):
             string.ascii_uppercase if last_letter_upper else string.ascii_lowercase
         )
 
-        print(mistaken_char)
         pyautogui.typewrite(mistaken_char)
-        make_time_delay_wrong()
+        time.sleep(make_time_delay_wrong())
 
     for x in range(amount_of_mistakes):
         pyautogui.press("backspace")
-        random.uniform(
-            0.2, 0.5
-        )  # correct mistakes with a delay between 0.2 and 0.5 seconds.
+        time.sleep(random.uniform(0, 0.1))
 
 
 def type_char_x(x):
@@ -79,25 +74,21 @@ def is_capslock_on():
 def type_word(word):
 
     for x in word:
+
+        type_char_x(x)
+        time.sleep(make_time_delay_normal())
+
         mistakes = do_mistakes_or_not()
 
         if mistakes:
             make_mistakes(x)
 
-        else:
-            type_char_x(x)
-            make_time_delay_normal()
-
 
 def do_mistakes_or_not():
-    return random.choices([True, False], weights=[10, 90], k=1)[0]
+    return random.choices([True, False], weights=[5, 95], k=1)[0]
 
 
-print(make_time_delay_wrong())
-
-
-#type_word("Hello bro!, HOW are you?")
-make_mistakes("0")
-
-
-# print(do_mistakes_or_not())
+# make_mistakes("r")
+type_word(
+    "Growing up as the child of a librarian, I was surrounded by books. Pulitzer finalist Lucille Clifton’s affirming words, Gwendolyn Brooks’ tales of Chicago, and Langston Hughes’ lush letters figured prominently in my home. We had quite a bit of Rumi, Shel Silverstein, and Carl Sandburg added in for good measure."
+)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
- bro!, HOW are you?
+type_word(
+#     "Growing up as the child of a librarian, I was surrounded by books. Pulitzer finalist Lucille Clifton’s affirming words, Gwendolyn Brooks’ tales of Chicago, and Langston Hughes’ lush letters figured prominently in my home. We had quite a bit of Rumi, Shel Silverstein, and Carl Sandburg added in for good measure."
+# )
