1d9f055a52b372c59327f9ba5a0c089fddf32b49
Bambara123 <e19034@eng.pdn.ac.lk>
2024-04-24 22:42:57 +0530

    read_numbers_or_float

--- test3.py
+++ test3.py
@@ -1,57 +1,10 @@
-import random
+import read_number_or_float as rnf
+import time
+
+region = (244, 613, 141, 57)
 
 
-# generate random amount of points between two points - which represents abnormal behavior.
-def generate_points_abnormal(point_1, point_2):
-    num_points = random.choices(
-        [1, 2, 3, random.randint(4, 10)], weights=[59, 20, 20, 1], k=1
-    )[0]
-    points = []
+time.sleep(3)
 
-    for _ in range(num_points):
-        x = random.randint(min(point_1[0], point_2[0]), max(point_1[0], point_2[0]))
-        y = random.randint(min(point_1[1], point_2[1]), max(point_1[1], point_2[1]))
-        point = (x, y)
-        points.append(point)
-
-    return points
-
-
-# generate random amount of points between two points - which represents normal behavior.
-def generate_points_normal(point_1, point_2):
-    num_points = random.choices(
-        [0, 1, 2, 3, random.randint(4, 10)], weights=[69, 25, 10, 5, 1], k=1
-    )[0]
-    points = []
-
-    if num_points > 0:
-        last_point = point_1
-        for _ in range(num_points):
-            x = random.randint(
-                min(last_point[0] + 1, point_2[0]), max(last_point[0] + 1, point_2[0])
-            )
-            y = random.randint(
-                min(last_point[1] + 1, point_2[1]), max(last_point[1] + 1, point_2[1])
-            )
-            point = (x, y)
-            points.append(point)
-            last_point = point
-
-    return points
-
-
-# generate random amount of points in anywhere on screen.
-def generate_points_anywhere(point_1=(0, 0), point_2=(1920, 1080)):
-    num_points = random.choices([1, 2, 3], weights=[60, 20, 20], k=1)[0]
-    points = []
-
-    for _ in range(num_points):
-        x = random.randint(min(point_1[0], point_2[0]), max(point_1[0], point_2[0]))
-        y = random.randint(min(point_1[1], point_2[1]), max(point_1[1], point_2[1]))
-        point = (x, y)
-        points.append(point)
-
-    return points
-
-
-print(generate_points_anywhere())
+numbers = rnf.capture_screen_and_identify_numbers(region)
+print("Numbers identified in the specified region:", numbers)
